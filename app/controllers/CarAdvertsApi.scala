71fa65d8e5fcd3c5d4aa3dfbad9bc766c85301c2
Carsten Blank <blank@cncengine.com>
2015-12-22 21:03:03 +0100

    DynanmoDb Repository with dependency injection.

--- app/controllers/CarAdvertsApi.scala
+++ app/controllers/CarAdvertsApi.scala
@@ -1,9 +1,12 @@
 package controllers
 
+import javax.inject.Inject
+
+import logic.DynamoDbRepository
 import play.api._
 import play.api.mvc._
 
-class CarAdvertsApi extends Controller {
+class CarAdvertsApi @Inject() (repository: DynamoDbRepository) extends Controller  {
   def list = Action {
     Ok
   }
